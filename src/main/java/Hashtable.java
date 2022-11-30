feb3338d7a605b71ae8f08b3e14367a52909856c
Tim Tommins <tom.timmons@outlook.com>
2022-11-30 01:38:47 +0000

    table passes all tests

--- src/main/java/Hashtable.java
+++ src/main/java/Hashtable.java
@@ -1,37 +1,74 @@
 public class Hashtable {
-    double loadFactor = 0.0;
     int capacity;
-    Object[] table;
+    String[] table;
 
 
     public Hashtable(int size) {
         // Add code here for your constructor
         this.capacity = size;
-        this.table = new Object[size];
+        this.table = new String[size];
     }
 
     public int hashValue(String key) {
         // calculates the index using a hashValue and hashFunction
         int hashCode = key.hashCode();
-        return (hashCode & 0x7fffffff) % capacity;
+        return (hashCode & 0x7fffffff) % this.capacity;
     }
 
     public void add(String key) {
         // Add code here for your add method, using linear probing to resolve collisions
+        int index = hashValue(key);
+        for (int i = 0; i < this.capacity; i++) {
+            int linearProbeIndex = (index + i) % this.capacity;
+            if (key.equals(this.table[linearProbeIndex])) { return; }
+            if (this.table[linearProbeIndex] == null) {
+                this.table[linearProbeIndex] = key;
+                return;
+            }
+        }
+    }
+
+    private int stringSearch(String key) {
+        // Searches for string and returns its index, returns -1 if not found
+        int index = hashValue(key);
+        for (int i = 0; i < this.capacity; i++) {
+            int linearProbeIndex = (index + i) % this.capacity;
+            if (key.equals(this.table[linearProbeIndex])) { return linearProbeIndex; }
+        }
+        return -1;
     }
 
     public boolean search(String key) {
-        // Add code here for your search method
-        return false;
+        // Returns true if key is in the table
+        return stringSearch(key) >= 0;
     }
 
     public void delete(String key) {
-        // Add code here for your delete method
+        // Deletes the key from the table and rehashes
+        int index = stringSearch(key);
+        if (index >= 0) {
+            this.table[index] = null;
+            rehash(index);
+        }
+    }
+
+    private void rehash(int startIndex) {
+        // rehashes the hash table
+        String[] temp = new String[this.capacity];
+        System.arraycopy(this.table, 0, temp, 0, this.capacity);
+
+        this.table = new String[this.capacity];
+        for (int i = 0; i < this.capacity; i++) {
+            int linearProbeIndex = (startIndex + i) % this.capacity;
+            if (temp[linearProbeIndex] != null) {
+                this.add(temp[linearProbeIndex]);
+            }
+        }
     }
 
     public String get(int i) {
         // Add code here for your get method
-        return null;
+        return this.table[i];
     }
 
 }
